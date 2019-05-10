3a0938508280d297ad4dc6dac16d449bda58d455
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-10 13:11:30 -0400

    generic router set up

---
+++ api/router/projectsRouter.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const db = require(`./projectModels`);
+
+//=====================================Create Router
+router.post("/", async (req, res) => {})
+//=====================================Read Router
+router.get("/", async (req, res) => {})
+//-------------------------------By ID
+router.get("/", async (req, res) => {})
+//=====================================Update Router
+router.put("/", async (req, res) => {})
+//=====================================Delete Router
+router.delete("/", async (req, res) => {})
+
+module.exports = router;
